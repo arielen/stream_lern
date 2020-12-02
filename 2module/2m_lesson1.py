20418f8aa04101b38914bd3cbf95babef019a865
arielen <79134475418@mail.ru>
2020-12-03 01:18:46 +0700

    initial commit

---
+++ 2module/2m_lesson1.py
@@ -0,0 +1,16 @@
+""" Найдите в программе необъявленную переменную и объявите её, присвоив ей значение ‘Кот’.
+
+client = 'Петя'
+print(client)
+print(' и ')
+print(pet) """
+
+pet = 'Кот'
+client = 'Петя'
+print(client)
+print(' и ')
+print(pet)
+
+print('-------------')
+
+print(client, 'и', pet)
---
+++ 2module/2m_lesson2.py
@@ -0,0 +1,6 @@
+""" Red Blue Green RedGreenBlue Blue GreenBlue """
+r = 'Red'
+g = 'Green'
+b = 'Blue'
+
+print(r, b, g, r + g + b, b, g + b)
